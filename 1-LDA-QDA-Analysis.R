bb3ad0033009c4fc823d0c4740f06c55a04380c3
Md Arafat Hossain Khan <46831086+MdArafatHKhan@users.noreply.github.com>
2020-12-23 08:14:51 -0600

    Update and rename 1-LDA-Analysis.R to 1-LDA-QDA-Analysis.R

--- 1-LDA-QDA-Analysis.R
+++ 1-LDA-QDA-Analysis.R
@@ -1,4 +1,4 @@
-##################### LDA ########################
+##################### LDA-QDA ########################
 #Install if the package doesn't exist 
 if (!require(ISLR)) install.packages('ISLR')
 library(ISLR) 
@@ -19,6 +19,7 @@ testDatagroup3 = group3[(nrow(group3)-testDataFronEachGroup+1):nrow(group3),]
 trainingData = rbind(trainingDatagroup1, trainingDatagroup2, trainingDatagroup3)
 testData = rbind(testDatagroup1, testDatagroup2, testDatagroup3)
 
+############ LDA ############
 lda.fit = lda(Group~GPA+GMAT, data=trainingData)
 lda.fit
 plot(lda.fit)
@@ -53,3 +54,32 @@ lda.model <- lda(Group ~ ., data = trainingData)
 lda.model
 our.lda.fit$disc
 lda.pred <- predict(our.lda.fit, trainingData)
+
+
+############# QDA ####################
+qda.fit = qda(Group~GPA+GMAT, data=trainingData)
+qda.fit
+qda.pred = predict(qda.fit, trainingData)
+class(qda.pred)
+table(qda.pred$class,trainingData$Group)
+source("decisionplot.R")
+plot(trainingData$GPA,trainingData$GMAT)
+decisionplot(qda.fit, trainingData, class = "Group", main = "qda")
+legend("topleft", pch=c(3,2,4), col=c("green", "red","blue"), c("Group 1", "Group 2", "Group 3"))
+1-mean(qda.pred$class == trainingData$Group)
+qda.pred = predict(qda.fit, testData)
+class(qda.pred)
+table(qda.pred$class,testData$Group)
+source("decisionplot.R")
+plot(testData$GPA,testData$GMAT)
+decisionplot(qda.fit, testData, class = "Group", main = "qda")
+legend("topleft", pch=c(3,2,4), col=c("green", "red","blue"), c("Group 1", "Group 2", "Group 3"))
+1-mean(qda.pred$class == testData$Group)
+
+
+our.qda.fit <- s6340.qda(as.factor(admission.decision), admission.predictors)
+our.qda.fit
+########### Equation for decision boundary ###############
+our.qda.fit$quad.coef.matrix #Matrix for quadratic terms
+our.qda.fit$linear.coef.vector #vector for linear terms
+our.qda.fit$cutoff #cutoff value
